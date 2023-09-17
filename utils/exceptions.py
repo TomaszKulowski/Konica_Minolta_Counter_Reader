405831498d24db4cb3da03818cbc5a573353683c
Tomasz Kulowski <tomek987125@gmail.com>
2023-09-17 19:36:25 +0200

    Refactor add function

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -8,3 +8,9 @@ class ReportError(Exception):
     """
     Exception raised when an error occurs while generating or processing a report.
     """
+
+
+class AddAutostartError(Exception):
+    """
+    Exception raised when an error occurs while adding an entry to the autostart.
+    """
